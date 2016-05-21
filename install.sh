cd19cf4bc8d1fd17128a14350a8bf007f13e787b
Swiiip <hugo.duthil@gmail.com>
2016-05-21 17:41:45 +0200

    remove command tools installation

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Instal all software updates
-softwareupdate --install --all
-
-# XCode command line tools
-xcode-select --install
+# XCode command line tools must be installed
+#xcode-select --install
 
 # Brew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
