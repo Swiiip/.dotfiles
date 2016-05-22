60d1871b94c0f760b7d5741083f7833257ab7743
Swiiip <hugo.duthil@gmail.com>
2016-05-22 05:36:04 +0200

    fix curl cmd

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,5 +1,2 @@
 # Get plug.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Download nerd font
-curl -fLo ~/Library/Fonts/KnackRegularNerdFontComplete --create-dirs "https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/Hack/Regular/complete/Knack%20Regular%20Nerd%20Font%20Complete.ttf"
