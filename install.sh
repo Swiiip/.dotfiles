1a3ad77b5ac37f0fa3e63c439dac3e84c2ae8d09
Swiiip <hugo.duthil@gmail.com>
2016-05-22 03:35:02 +0200

    add brew.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [ -d "$DIR/.git" ] && git --work-tree="$DIR" --git-dir="$DIR/.git" pull origin master
 
 # Symlinks
-ln -sfv "$DIR/neovim/init.vim" "~/.config/nvim/init.vim"
+mkdir -p ~/.config/nvim
+ln -sfv $DIR/neovim/init.vim ~/.config/nvim/init.vim
 
 # Install
 . "$DIR/install/brew.sh"
