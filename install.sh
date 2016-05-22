eb24bdc0eccb955a1d201c2e2d2ae0942ebbd9bb
Swiiip <hugo.duthil@gmail.com>
2016-05-22 02:29:11 +0200

    Update neovim

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Update dotfiles itself first
 [ -d "$DIR/.git" ] && git --work-tree="$DIR" --git-dir="$DIR/.git" pull origin master
 
+# Symlinks
+ln -sfv "$DIR/neovim/init.vim" "~/.config/nvim/init.vim"
+
 # Install
 . "$DIR/install/brew.sh"
 . "$DIR/install/brew-cask.sh"
